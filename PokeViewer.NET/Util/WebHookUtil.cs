a8386f8d627d6903f439a75934990c38d13becd9
shinyhuntet <huoye@DESKTOP-JMLRMQM>
2025-07-22 22:07:59 +0900

    Minor Fix

--- PokeViewer.NET/Util/WebHookUtil.cs
+++ PokeViewer.NET/Util/WebHookUtil.cs
@@ -88,7 +88,7 @@ public static class WebHookUtil
     {
         GameStrings Strings = GameInfo.GetStrings("en");
         string userContent = pinguser ? $"<@{Settings.Default.UserDiscordID}>\n{Settings.Default.PingMessage}" : "";
-        string title = pinguser ? $"Match Found!" : isStaic ? $"Static Pokemon Found!{Environment.NewLine}Encounter #{Encounter}, Target Rate: {Rate:0.00}" : "Unwanted match..";
+        string title = pinguser ? $"Match Found!" : isStaic ? $"Static Pokemon Found!{Environment.NewLine}Encounter #{Encounter}, Target Rate: {Rate:0.00}%" : "Unwanted match..";
         string MarkString = "";
         string EmojiMark = MarkEmoji(pk, out MarkString);
         Span<int> _ivs = stackalloc int[6];
